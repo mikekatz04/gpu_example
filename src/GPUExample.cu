b138aace28424d9c6f4b406b204c3d8c4a9076aa
Michael Katz <mikekatz04@gmail.com>
2021-11-23 15:39:43 +0100

    bug fix

--- src/GPUExample.cu
+++ src/GPUExample.cu
@@ -3,31 +3,6 @@
 #include "math.h"
 
 
-#ifdef __CUDACC__
-#define CUDA_CALLABLE_MEMBER __device__
-#define CUDA_KERNEL __global__
-#define CUDA_SHARED __shared__
-#define CUDA_SYNC_THREADS __syncthreads()
-#else
-#define CUDA_CALLABLE_MEMBER
-#define CUDA_KERNEL
-#define CUDA_SHARED
-#define CUDA_SYNC_THREADS
-#endif
-
-#ifdef __CUDACC__
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
-
-#endif
-
 CUDA_KERNEL
 void build_sine_waves(double* output, double* amplitudes, double* frequencies, double* phases, double dt, int num_t, int num_modes)
 {
